824ae680b29eebbe8fdde146099941281b21aa48
SolyomSzabi <solymszabi@gmail.com>
2023-03-17 11:47:24 +0200

    add more images and made it work with jpg, not just jpeg

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function search() {
   const query = document.getElementById("searchbar").value;
   const resultsDiv = document.getElementById("results");
-  
-  fetch(`http://localhost:8080/img/${query}.jpeg`)
+
+  Promise.race([
+    fetch(`http://localhost:8080/img/${query}.jpeg`),
+    fetch(`http://localhost:8080/img/${query}.jpg`)
+  ])
     .then(response => {
       if (response.ok) {
         const img = document.createElement("img");
-        img.src = `http://localhost:8080/img/${query}.jpeg`;
+        img.src = response.url;
         resultsDiv.innerHTML = "";
         resultsDiv.appendChild(img);
       } else {
